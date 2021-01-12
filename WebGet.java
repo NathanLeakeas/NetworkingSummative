f8cacb922275d083d78bcf6cbbe625f9b37b92a8
DanielLott4 <54151017+DanielLott4@users.noreply.github.com>
2021-01-11 21:16:57 -0600

    Created WebGet
    Modified the WebGet program to only print the HTTP header of the returned HTML page.

---
+++ WebGet.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.*;
+
+public class WebGet
+{
+    public static void main(String[] args) throws IOException
+    {
+        String host;
+        String resource;
+
+        if (args.length == 2)
+        {
+            host = args[0];
+            resource = args[1];
+        }
+        else
+        {
+            System.out.println("Getting / from horstmann.com");
+            host = "horstmann.com";
+            resource = "/";
+        }
+
+        //Open socket
+        final int HTTP_PORT = 80;
+        try (Socket s = new Socket(host, HTTP_PORT))
+        {
+            //Get streams
+            InputStream instream = s.getInputStream();
+            OutputStream outstream = s.getOutputStream();
+
+            //Turn streams into scanners and writers
+            Scanner in = new Scanner(instream);
+            PrintWriter out = new PrintWriter(outstream);
+
+            //Send command
+            String command = "GET " + resource + " HTTP/1.1\n"
+                + "Host: " + host + "\n\n";
+            out.print(command);
+            out.flush();
+
+            //Read server response
+            int count = 0;
+            while (in.hasNextLine()&&count<8)
+            {
+                String input = in.nextLine();
+                System.out.println(input);
+                count++;
+            }
+        }
+
+    }
+}
